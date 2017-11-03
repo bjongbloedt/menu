67a6c9e0fe7fddaa1675e532dd6d180e2d2c04d7
Ben Jongbloedt <bjongbloedt@users.noreply.github.com>
2017-11-03 14:52:15 -0500

    adding routes and tests

--- api/app.py
+++ api/app.py
@@ -1,18 +1,30 @@
 from apistar import Include, Route
 from apistar.frameworks.wsgi import WSGIApp as App
-from apistar.handlers import docs_urls, static_urls
+from apistar.handlers import docs_urls
 
+def ping():
+    """
+    Healthcheck endpoint
+    """
+    return {'message': 'ok'}
 
-def welcome(name=None):
-    if name is None:
-        return {'message': 'Welcome to API Star!'}
-    return {'message': 'Welcome to API Star, %s!' % name}
+def get_menu_by_id(menu_id):
+    """
+    Gets a specific menu by id
+    """
+    return {'message': f'Got menu {menu_id}'}
 
+def get_items_for_menu(menu_id):
+    """
+    Gets all of the items for the given menu_id
+    """
+    return {'message': f'Got items for menu {menu_id}'}
 
 routes = [
-    Route('/', 'GET', welcome),
-    Include('/docs', docs_urls),
-    Include('/static', static_urls)
+    Route('/menu/v1/menus/{menu_id}', 'GET', get_menu_by_id),
+    Route('/menu/v1/menus/{menu_id}/items', 'GET', get_items_for_menu),
+    Route('/healthz', 'GET', ping),
+    Include('/docs', docs_urls)
 ]
 
 app = App(routes=routes)
--- api/tests.py
+++ api/tests.py
@@ -1,20 +1,51 @@
 from apistar.test import TestClient
-from app import app, welcome
+from app import app, get_items_for_menu, get_menu_by_id, ping
 
 
-def test_welcome():
+def test_ping():
     """
-    Testing a view directly.
+    Test ping
     """
-    data = welcome()
-    assert data == {'message': 'Welcome to API Star!'}
+    data = ping()
+    assert data == {'message': 'ok'}
 
-
-def test_http_request():
+def test_ping_request():
     """
-    Testing a view, using the test client.
+    Test ping
     """
     client = TestClient(app)
-    response = client.get('http://localhost/')
+    response = client.get('http://localhost/healthz')
     assert response.status_code == 200
-    assert response.json() == {'message': 'Welcome to API Star!'}
+    assert response.json() == {'message': 'ok'}
+
+def test_get_menu_by_id():
+    """
+    Test getting a single menu
+    """
+    data = get_menu_by_id('1')
+    assert data == {'message': 'Got menu 1'}
+
+def test_get_menu_by_id_request():
+    """
+    Test getting a single menu
+    """
+    client = TestClient(app)
+    response = client.get('http://localhost/menu/v1/menus/1')
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Got menu 1'}
+
+def test_get_items_for_menu():
+    """
+    Test getting all items for a menu
+    """
+    data = get_items_for_menu('1')
+    assert data == {'message': 'Got items for menu 1'}
+
+def test_get_menu_by_id_request():
+    """
+    Test getting a single menu
+    """
+    client = TestClient(app)
+    response = client.get('http://localhost/menu/v1/menus/1/items')
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Got items for menu 1'}
