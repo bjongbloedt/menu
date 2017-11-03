b0a6717ae929c3bc2b38c48c92c4a9ba9e200973
Ben Jongbloedt <bjongbloedt@users.noreply.github.com>
2017-11-03 15:09:41 -0500

    getting schemas figured out

--- api/app.py
+++ api/app.py
@@ -1,6 +1,31 @@
+import typing
 from apistar import Include, Route
 from apistar.frameworks.wsgi import WSGIApp as App
 from apistar.handlers import docs_urls
+from apistar import typesystem
+
+
+class Item(typesystem.Object):
+    """
+    Menu item schema
+    """
+    properties = {
+        'id': typesystem.String,
+        'name': typesystem.String,
+        'price': typesystem.Number,
+        'image': typesystem.String,
+        'section': typesystem.String
+    }
+
+class Menu(typesystem.Object):
+    """
+    Menu schema
+    """
+    properties = {
+        'id': typesystem.String,
+        'name': typesystem.String,
+        'description': typesystem.String
+    }
 
 def ping():
     """
@@ -8,17 +33,19 @@ def ping():
     """
     return {'message': 'ok'}
 
-def get_menu_by_id(menu_id):
+def get_menu_by_id(menu_id: str) -> typing.List[Menu]:
     """
     Gets a specific menu by id
     """
-    return {'message': f'Got menu {menu_id}'}
+    return Menu(name="my first menu", id=menu_id, description="The best first menu")
 
-def get_items_for_menu(menu_id):
+def get_items_for_menu(menu_id: str) -> typing.List[Item]:
     """
     Gets all of the items for the given menu_id
     """
-    return {'message': f'Got items for menu {menu_id}'}
+    return [
+        Item(id='1', name='A menu item', price=9.99, image='http://google.com', section='dessert')
+    ]
 
 routes = [
     Route('/menu/v1/menus/{menu_id}', 'GET', get_menu_by_id),
--- api/tests.py
+++ api/tests.py
@@ -9,6 +9,7 @@ def test_ping():
     data = ping()
     assert data == {'message': 'ok'}
 
+
 def test_ping_request():
     """
     Test ping
@@ -18,34 +19,44 @@ def test_ping_request():
     assert response.status_code == 200
     assert response.json() == {'message': 'ok'}
 
+
 def test_get_menu_by_id():
     """
     Test getting a single menu
     """
     data = get_menu_by_id('1')
-    assert data == {'message': 'Got menu 1'}
+    assert data == {'id': '1', 'name': 'my first menu',
+                    'description': 'The best first menu'}
+
 
 def test_get_menu_by_id_request():
     """
     Test getting a single menu
     """
     client = TestClient(app)
-    response = client.get('http://localhost/menu/v1/menus/1')
+    response = client.get('http://localhost/menu/v1/menus/3')
     assert response.status_code == 200
-    assert response.json() == {'message': 'Got menu 1'}
+    assert response.json() ==\
+        {'id': '3', 'name': 'my first menu', 'description': 'The best first menu'}
+
 
 def test_get_items_for_menu():
     """
     Test getting all items for a menu
     """
     data = get_items_for_menu('1')
-    assert data == {'message': 'Got items for menu 1'}
+    assert data ==\
+        [{'id': '1', 'name': 'A menu item', 'price': 9.99,
+          'image': 'http://google.com', 'section': 'dessert'}]
 
-def test_get_menu_by_id_request():
+
+def test_get_items_for_menu_request():
     """
     Test getting a single menu
     """
     client = TestClient(app)
     response = client.get('http://localhost/menu/v1/menus/1/items')
     assert response.status_code == 200
-    assert response.json() == {'message': 'Got items for menu 1'}
+    assert response.json() ==\
+        [{'id': '1', 'name': 'A menu item', 'price': 9.99,
+          'image': 'http://google.com', 'section': 'dessert'}]
